fbb89f509ced7f393c52afb087eb6ea136471fc4
mtnbutas <m.trisha.n.b@gmail.com>
2018-07-10 10:37:07 +0800

    changed Tweet likes field into favorites

--- tweets/models.py
+++ tweets/models.py
@@ -11,7 +11,7 @@ class Tweet(models.Model):
 	slug = models.CharField(max_length = 500)
 	content = models.TextField(blank=False)
 	create_date = models.DateTimeField(auto_now_add = True)
-	likes = models.PositiveIntegerField(default=0)
+	favorites = models.PositiveIntegerField(default=0)
 	retweet = models.PositiveIntegerField(default=0)
 	root = models.ForeignKey(Tweet, related_name='root_tweet')
 	parent = models.ForeignKey(Tweet, related_name='parent_tweet')
